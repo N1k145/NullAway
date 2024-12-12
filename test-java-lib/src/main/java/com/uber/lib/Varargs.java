96d224439233b824756e4cdda87fc1c03b41e24e
Kellner, Niklas <Niklas.Kellner@vector.com>
2024-12-12 07:21:36 +0100

    reproducer

--- test-java-lib/src/main/java/com/uber/lib/Varargs.java
+++ test-java-lib/src/main/java/com/uber/lib/Varargs.java
@@ -4,7 +4,15 @@ import javax.annotation.Nullable;
 
 public class Varargs {
 
+  @Nullable private static final String[] test = null;
+
   public Varargs(@Nullable String... args) {}
 
   public static void typeUse(String @org.jspecify.annotations.Nullable ... args) {}
+
+  public static void typeUseNullableElementsInit(@Nullable String val) {
+    new Varargs().typeUseNullableElements(val, test);
+  }
+
+  public void typeUseNullableElements(@Nullable String test, @Nullable String... args) {}
 }
